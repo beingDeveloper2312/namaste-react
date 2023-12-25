e6d16872b91bbd84f75dfb540a31df278622c519
Rohit Jaiswal <jaiswalrohit@gmail.com>
2023-12-26 01:09:35 +0530

    Episode-1 Inception

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+  "div",
+  {
+    id: "heading",
+    xyz: "abc",
+  },
+  [React.createElement("div", {}, [React.createElement("h1", {}, "fsfsdf"), React.createElement("h2", {}, "dfsd")]), React.createElement("div", {}, [React.createElement("h1", {}, "fsfsdf"), React.createElement("h2", {}, "dfsd")])]
+);
+
+console.log(heading); // an object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
